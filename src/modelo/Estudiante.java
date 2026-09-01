459670927677dcd87454fd5d8692f000449e48d7
Miguel <mmurrugarra@perufarma.com.pe>
2026-09-01 09:51:36 -0500

    creación de la clase Estudiantes

--- src/modelo/Estudiante.java
+++ src/modelo/Estudiante.java
@@ -1,14 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author murtormi
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Estudiante {
     
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private List<Calificacion> calificaciones;
+
+    public Estudiante(String codigo, String nombre, String apellido) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.calificaciones = new ArrayList<>(); // Inicializa la lista vacía
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public List<Calificacion> getCalificaciones() {
+        return calificaciones;
+    }
+
+    public void agregarCalificacion(Calificacion calificacion) {
+        this.calificaciones.add(calificacion);
+    }
+
+    public boolean tieneCalificaciones() {
+        return !this.calificaciones.isEmpty();
+    }
+
+    public double calcularPromedio() {
+        if (!tieneCalificaciones()) {
+            return 0.0;
+        }
+        
+        double suma = 0.0;
+        for (Calificacion c : calificaciones) {
+            suma += c.getNota();
+        }
+        
+        return suma / calificaciones.size();
+    }
+
+    @Override
+    public String toString() {
+        return "[" + codigo + "] " + nombre + " " + apellido + " | Calificaciones registradas: " + calificaciones.size();
+    }
 }
