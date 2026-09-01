2399afaa20feb0bc95a373c23496916ace7d90e5
Miguel <mmurrugarra@perufarma.com.pe>
2026-09-01 09:50:04 -0500

    creación de la clase Calificación

--- src/modelo/Calificacion.java
+++ src/modelo/Calificacion.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author murtormi
- */
 public class Calificacion {
+    
+    private String curso;
+    private double nota;
+    
+    public Calificacion(String curso, double nota){
+        this.curso = curso;
+        this.nota = nota;
+    }
+    
+    public String getCurso(){
+     return curso;
+    }
+    
+    public double getNota(){
+        return nota;
+    }
+    
+    @Override // remplazamos toString() que viene de Object.
+    public String toString() {
+        return curso + ": " + nota;
+    }
+    
     
 }
